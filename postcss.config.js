c3555d69831548049bf2fa7b00d8aea5aa87588e
Aayush Kumar <161928973+Aayush-K35@users.noreply.github.com>
2025-11-24 22:24:30 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+    plugins: {
+        // UPDATED to use the explicit name suggested by the error message.
+        '@tailwindcss/postcss': {},
+        autoprefixer: {},
+    },
+}
